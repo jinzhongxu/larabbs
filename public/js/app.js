4a100db3f56430f111d100f71a8ef7d996903167
zhongxu <1378674594@qq.com>
2020-08-13 02:50:11 +0000

    回复列表

--- public/js/app.js
+++ public/js/app.js
@@ -37341,8 +37341,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/zhongxujin/Desktop/Code/larabbs/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/zhongxujin/Desktop/Code/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Code/larabbs/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/Code/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
