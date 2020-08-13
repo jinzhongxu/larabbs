24ecb6863c46ceaec585f9cccfc9ea62010ca4a8
zhongxu <1378674594@qq.com>
2020-08-13 03:57:57 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,11 +12,19 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        $reply->user_content = clean($reply->user_content, 'user_topic_body');
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)
     {
         //
+    }
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 }
